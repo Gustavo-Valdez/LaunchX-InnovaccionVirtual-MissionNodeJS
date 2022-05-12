831d3fdcb82ecb1915bf50851f3af56740cec806
Gustavo-Valdez <86739150+Gustavo-Valdez@users.noreply.github.com>
2022-05-12 14:34:42 -0500

    Pruebas de Testeo

--- weekly_mission_2/example5/pokemon.js
+++ weekly_mission_2/example5/pokemon.js
@@ -2,21 +2,7 @@
 Este ejemplo incluye dependencias. En tu línea de comando ve a este proyecto y ejecuta el comando `npm install` para descargar las dependencias, esto creará una carpeta node_modules
 */
 export default class Pokemon {
-  constructor(name, type, age){
-    this.name = name
-    this.type = type
-    this.age = age
-    this.attacks = []
-  }
-
-  get getAttacks(){
-    return this.attacks
-  }
-
-  set setAttacks(attacks){
-    return this.attacks = attacks
+  constructor(name) {
+    this.name = name;
   }
 }
-
-const myPokemon = new Pokemon("Charmander", "Fuego", 10)
-console.log(myPokemon)
